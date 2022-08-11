9948435bddb59338975ad3f865d437f2cbbdbee2
raj <dharmarajp@rba.gov.au>
2022-08-11 15:53:12 +1000

    working devcontainer and Dockerfile added gitignore able to compile stuff in remote containter using VS Code working blink program

---
+++ blink.c
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() 
+{
+
+    const uint LED_PIN = 25;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    stdio_init_all();
+
+    while (true) 
+    {
+        printf("Led! ON\r\n");
+        gpio_put(LED_PIN, 1);
+        sleep_ms(1000);
+        printf("Led! Test OFF\r\n");        
+        gpio_put(LED_PIN, 0);
+        sleep_ms(1000);
+    }
+
+
+    return 0;
+
+}
